e921a23f23cbf748d2cc3de3f3c4781deacacaaf
x-xinyue <140133640+x-xinyue@users.noreply.github.com>
2025-10-17 11:34:46 +0800

    Work in progress

--- js/charts.js
+++ js/charts.js
@@ -5,7 +5,26 @@ vegaEmbed('#choropleth_map', vg_1).then(function(result) {
 }).catch(console.error);
 
 // Bar chart
-var vg_2 = "js/bar_chart.json";
-vegaEmbed('#bar_chart', vg_2).then(function(result) {
+var vg_2 = "js/line_chart.json";
+vegaEmbed('#line_chart', vg_2, { actions: false, config: { autosize: { type: "fit", contains: "padding" }}}).then(function(result) {
+  // success
+}).catch(console.error);
+
+// streamgraph
+var vg_3 = "js/streamgraph.json";
+vegaEmbed('#streamgraph', vg_3, { actions: false, config: { autosize: { type: "fit", contains: "padding" }}}).then(function(result) {
+  // success
+}).catch(console.error);
+
+
+// bump_chart
+var vg_4 = "js/bump_chart.json";
+vegaEmbed('#bump_chart', vg_4, { actions: false, config: { autosize: { type: "fit", contains: "padding" }}}).then(function(result) {
+  // success
+}).catch(console.error);
+
+// slope_chart
+var vg_5 = "js/slope_chart.json";
+vegaEmbed('#slope_chart', vg_5, { actions: false, config: { autosize: { type: "fit", contains: "padding" }}}).then(function(result) {
   // success
 }).catch(console.error);
