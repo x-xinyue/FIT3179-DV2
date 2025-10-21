5d00dcf22db197c76f5b5c17e9f6cddd3d828747
x-xinyue <140133640+x-xinyue@users.noreply.github.com>
2025-10-21 15:55:17 +0800

    Completed

--- js/charts.js
+++ js/charts.js
@@ -23,8 +23,14 @@ vegaEmbed('#bump_chart', vg_4, { actions: false, config: { autosize: { type: "fi
   // success
 }).catch(console.error);
 
-// slope_chart
-var vg_5 = "js/slope_chart.json";
-vegaEmbed('#slope_chart', vg_5, { actions: false, config: { autosize: { type: "fit", contains: "padding" }}}).then(function(result) {
+// bar_chart
+var vg_5 = "js/bar_chart.json";
+vegaEmbed('#bar_chart', vg_5, { actions: false, config: { autosize: { type: "fit", contains: "padding" }}}).then(function(result) {
+  // success
+}).catch(console.error);
+
+// stacked_bar
+var vg_6 = "js/stacked_bar_graph.json";
+vegaEmbed('#stacked_bar_graph', vg_6, { actions: false, config: { autosize: { type: "fit", contains: "padding" }}}).then(function(result) {
   // success
 }).catch(console.error);
